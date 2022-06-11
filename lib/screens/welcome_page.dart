31f7f08d0e8f8ab1150d892e2648676b2fe49ce0
Kagiso Marvin Molekwa <kagisomarvin@gmail.com>
2022-06-11 22:12:39 +0200

    Initial commit.

---
+++ lib/screens/welcome_page.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import '../constants.dart';
+import '../screens/screen.dart';
+import '../widgets/widget.dart';
+
+class WelcomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Flexible(
+                child: Column(
+                  children: [
+                    Center(
+                      child: Container(
+                        width: MediaQuery.of(context).size.width * 0.8,
+                        child: Image(
+                          image:
+                              AssetImage('assets/images/team_illustration.png'),
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text(
+                      "AutoInvestor Bot",
+                      style: kHeadline,
+                      textAlign: TextAlign.center,
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Text(
+                        "The AutoInvestor Bot streamlines the investment process by eliminating the need to assess current market circumstances before selecting an asset to invest in. The plugin will be more efficient than human investing and make fewer mistakes with minimal room for emotion. This will benefit the retail Investor because the stock market is very volatile.",
+                        style: kBodyText,
+                        textAlign: TextAlign.center,
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                height: 60,
+                decoration: BoxDecoration(
+                  color: Colors.grey[850],
+                  borderRadius: BorderRadius.circular(18),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: MyTextButton(
+                        bgColor: Colors.white,
+                        buttonName: 'Register',
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              CupertinoPageRoute(
+                                  builder: (context) => RegisterPage()));
+                        },
+                        textColor: Colors.black87,
+                      ),
+                    ),
+                    Expanded(
+                      child: MyTextButton(
+                        bgColor: Colors.transparent,
+                        buttonName: 'Sign In',
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              CupertinoPageRoute(
+                                builder: (context) => SignInPage(),
+                              ));
+                        },
+                        textColor: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
